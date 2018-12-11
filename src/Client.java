0e2c6dd37e70cfe0da182af4df9b5323a826c517
mhmdsmdi <mhmd.samadi76@gmail.com>
2018-12-11 14:32:42 +0330

    Calculator, Client  add

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,38 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+
+    private Socket clientSocket;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    private byte[] input;
+    private static final int MAX_INPUT_SIZE = 10000;
+
+    public Client() {
+        try {
+            clientSocket = new Socket("localhost", Server.SERVER_PORT);
+            inputStream = new ObjectInputStream(clientSocket.getInputStream());
+            outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            input = new byte[MAX_INPUT_SIZE];
+            sendPacketToServer(new Packet(OperatorType.Add, 2, 2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendPacketToServer(Packet packet) {
+        try {
+            System.out.println("asdsads");
+            outputStream.writeObject(packet);
+            outputStream.flush();
+            System.out.println("Packet Send to server");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,21 @@
-import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server extends Thread {
 
+    public static int SERVER_PORT = 8080;
     private ServerSocket serverSocket;
     private byte[] userByte;
     private static final int USER_MAX_INPUT_SIZE = 10000;
+    private Calculator calculator;
 
     public Server() {
         try {
-            serverSocket = new ServerSocket(8080);
+            serverSocket = new ServerSocket(SERVER_PORT);
             userByte = new byte[USER_MAX_INPUT_SIZE];
-
+            calculator = new Calculator();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -21,16 +23,31 @@ public class Server extends Thread {
 
     @Override
     public void run() {
+        System.out.println("server listen on port : " + SERVER_PORT);
         Socket connection = null;
         while (true) {
+            System.out.println("1");
             try {
                 connection = serverSocket.accept();
-                BufferedInputStream inputStream = new BufferedInputStream(connection.getInputStream());
-                inputStream.read(userByte);
+                System.out.println("Client accepted");
+                ObjectInputStream inputStream = new ObjectInputStream(connection.getInputStream());
+                System.out.println(":)");
+                try {
+                    Packet packet = (Packet) inputStream.readObject();
+                    System.out.println(packet.getOperator().name());
+
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
     }
 }
