310db3b3447df1428be8fd0b8b64a9e044b8d848
mhmdsmdi <mhmd.samadi76@gmail.com>
2018-12-11 13:46:17 +0330

    Calulator add

---
+++ src/Calculator.java
@@ -0,0 +1,34 @@
+public class Calculator {
+
+    public static double add(double op1, double op2) {
+        return op1 + op2;
+    }
+
+    public static double subtract(double op1, double op2) {
+        return op1 - op2;
+    }
+
+    public static double devide(double op1, double op2) {
+        return op1 / op2;
+    }
+
+    public static double multiple(double op1, double op2) {
+        return op1 * op2;
+    }
+
+    public static double sin(double op1) {
+        return Math.sin(op1);
+    }
+
+    public static double cos(double op1) {
+        return Math.cos(op1);
+    }
+
+    public static double tan(double op1) {
+        return Math.tan(op1);
+    }
+
+    public static double cot(double op1) {
+        return 1 / Math.tan(op1);
+    }
+}
